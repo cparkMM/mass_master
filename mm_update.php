fa42a6acae2d3bf148c34d25669e9dd776601a89
cparkMM <64803806+cparkMM@users.noreply.github.com>
2020-05-04 21:44:18 +0300

    Update mm_update.php

--- mm_update.php
+++ mm_update.php
@@ -1,1 +1,1 @@
-{"version_number":"10","version":"1.0","url":"https://github.com/cparkMM/mass_master/blob/master/SMI.lua"}
+{"version_number":"11","version":"1.1","url":"https://github.com/cparkMM/mass_master/blob/master/SMI.lua"}
